e1439597d0f048a3e32ce48f4fed328c9fe8d2ab
Rudraksh Kushwah <kushwahrudraksh3@gmail.com>
2026-05-18 00:45:58 +0530

    Registration and Login functionality

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogs',
     'about_links',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -132,3 +134,6 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- blog_main/views.py
+++ blog_main/views.py
@@ -2,7 +2,11 @@
 from django.shortcuts import render , redirect
 from blogs.models import Category, Blog
 from about_links.models import About, Links
-
+from . forms import *
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 
 def home(request):
@@ -25,3 +29,47 @@ def home(request):
         'about': about,
     }
     return render(request , 'home.html', context)
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        else:
+            print(form.errors)
+    else:
+        form = RegistrationForm()
+    
+    context = {
+        'form':form,
+    }
+    return render(request, 'register.html' ,context)
+
+
+
+
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('home')
